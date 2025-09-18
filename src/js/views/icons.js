def35ebe78d665c26b4966e3594bbadb5f821cc3
xWolfYx <david.skliarov.dev@gmail.com>
2025-09-18 22:39:59 +0400

    feat: add ogin img

--- src/js/views/icons.js
+++ src/js/views/icons.js
@@ -8,5 +8,13 @@ import chevronDown from "url:../../img/chevron-down.svg";
 import chevronLeft from "url:../../img/chevron-left.svg";
 import chevronRight from "url:../../img/chevron-right.svg";
 import union from "url:../../img/union.svg";
+import loginImg from "url:../../img/login-img.webp";
 
-export { adjustmentsHor, chevronDown, chevronLeft, chevronRight, union };
+export {
+  adjustmentsHor,
+  chevronDown,
+  chevronLeft,
+  chevronRight,
+  union,
+  loginImg,
+};
