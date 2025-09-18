7df6382060aed03f0f57d9131b0f6c9ab038642b
xWolfYx <david.skliarov.dev@gmail.com>
2025-09-18 18:50:10 +0400

    refactor: add info comments

--- src/js/views/icons.js
+++ src/js/views/icons.js
@@ -1,7 +1,11 @@
+/*
+ * These icons are used for the products page
+ * Any new that will be used on the products page should be imported here
+ */
+
 import adjustmentsHor from "url:../../img/adjustments-horizontal.svg";
 import chevronDown from "url:../../img/chevron-down.svg";
 import chevronLeft from "url:../../img/chevron-left.svg";
 import chevronRight from "url:../../img/chevron-right.svg";
-
 
 export { adjustmentsHor, chevronDown, chevronLeft, chevronRight };
